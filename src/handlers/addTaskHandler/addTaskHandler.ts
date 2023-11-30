94284b100468dcdcee34b814a82af0cb7bc3636e
AleksandrRazhev <alexandrrazh@gmail.com>
2023-11-30 21:43:24 +0300

    form data added

--- src/handlers/addTaskHandler/addTaskHandler.ts
+++ src/handlers/addTaskHandler/addTaskHandler.ts
@@ -5,8 +5,15 @@ export async function addTaskHandler(event: SubmitEvent) {
   event.preventDefault();
   const element: HTMLFormElement = this;
 
+  const formData: { [key: string]: string } = {};
+  new FormData(element).forEach((value, key) => {
+    if (typeof value !== "string") return;
+    formData[key] = value;
+  });
+
+  if (!("taskname" in formData)) return;
   const newTaskQuery = `mutation CreateTodo {
-createTodo(input:{title: "${element.taskname.value}", completed: false}){
+  createTodo(input:{title: "${formData.taskname}", completed: false}){
   title
   completed
   id
